80874d89ab5a9bf25925d37f4bd46c287ee99dbb
M. Alonso <port02.server@gmail.com>
2017-03-16 21:17:35 +0000

    update version

--- payment.py
+++ payment.py
@@ -105,7 +105,6 @@ class CondoPain(Workflow, ModelSQL, ModelView):
     @classmethod
     def __setup__(cls):
         super(CondoPain, cls).__setup__()
-        cls._order.insert(0, ('reference', 'DESC'))
         cls._error_messages.update({
                 'generate_error': ('Can not generate message "%s" of "%s"'),
                 })
@@ -346,7 +345,6 @@ class CondoPaymentGroup(ModelSQL, ModelView):
     @classmethod
     def __setup__(cls):
         super(CondoPaymentGroup, cls).__setup__()
-        cls._order.insert(0, ('reference', 'DESC'))
         t = cls.__table__()
         cls._sql_constraints += [
             ('reference_unique', Unique(t,t.company, t.reference),
@@ -464,11 +462,6 @@ class CondoPaymentGroup(ModelSQL, ModelView):
         table1 = pool.get('condo.payment.pain').__table__()
         table2 = cls.__table__()
 
-#TODO (remove hack)
-        user = Transaction().user
-        if user<=1:
-            return []
-
         if (operator=='=' and not value) or (operator=='!=' and value):
             query1 = table1.join(table2,
                             condition=table1.id == table2.pain).select(
@@ -493,107 +486,6 @@ class CondoPaymentGroup(ModelSQL, ModelView):
     def order_reference(tables):
         table, _ = tables[None]
         return [table.reference, table.company]
-
-    @classmethod
-    def PreparePaymentGroup(cls):
-        id = Transaction().context.get('company') or None
-
-        if id:
-            Company = Pool().get('company.company')
-            condo = Company.search([('id', '=', id),
-                                    ('is_Condominium', '=', True),
-                                    ('sepa_creditor_identifier', '!=', None),
-                                    ('party.active', '=', True),
-                                  ])
-
-            if (len(condo)==1 and len(condo[0].sepa_mandates)>0):
-                BankAccountNumber = Pool().get('bank.account.number')
-                bankaccountnumber = BankAccountNumber.search([('account.id', 'in', condo[0].party.bank_accounts),
-                                                              ('account.active', '=', True),
-                                                              ('type', '=', 'iban'),
-                                                             ])
-                if len(bankaccountnumber)==1: #Only condos with one bank account number
-                    Date = Pool().get('ir.date')
-                    d = Date.today()
-                    ddd = d.replace(day = 2,
-                                    month = d.month+1 if d.month<12 else 1,
-                                    year = d.year if d.month<12 else d.year+1)
-                    paymentgroup = cls(reference = '{:04d}'.format(ddd.year) +
-                                                   '_' +
-                                                   '{:02d}'.format(ddd.month) +
-                                                   '-' +
-                                                   bankaccountnumber[0].number_compact [8:12] +
-                                                   '.' +
-                                                   '{:04d}'.format(ddd.year)[-2:],
-                                       company = condo[0],
-                                       account_number = bankaccountnumber[0],
-                                       date = ddd + datetime.timedelta(days= 2 if ddd.weekday()>4 else 0),
-                                       sepa_charge_bearer = 'SLEV')
-                    paymentgroup.save()
-
-    @classmethod
-    def PrepareAllPaymentsGroup(cls):
-        pool = Pool()
-        banknumbers = pool.get('bank.account.number').__table__()
-        bankaccounts = pool.get('bank.account').__table__()
-        accountparties = pool.get('bank.account-party.party').__table__()
-        parties = pool.get('party.party').__table__()
-        companies = pool.get('company.company').__table__()
-
-        cursor = Transaction().cursor
-
-        #SELECT a.id, a.number, e.id, d.id, d.name FROM bank_account_number AS a
-        #    INNER JOIN bank_account AS b ON a.account=b.id
-        #    INNER JOIN "bank_account-party_party" AS c ON b.id=c.account
-        #    INNER JOIN party_party AS d ON c.owner=d.id
-        #    INNER JOIN company_company AS e ON d.id=e.party
-        # WHERE a.type='iban' AND b.active<>0 AND d.active<>0 AND e.is_Condominium<>0 AND e.sepa_creditor_identifier<>""
-        # GROUP BY e.id
-        # HAVING COUNT(e.id)=1
-        # ORDER BY e.id;
-        cursor.execute(*banknumbers.join(bankaccounts,
-                                     condition=banknumbers.account == bankaccounts.id).join(
-                                     accountparties,
-                                     condition=bankaccounts.id == accountparties.account).join(
-                                     parties,
-                                     condition=accountparties.owner == parties.id).join(
-                                     companies,
-                                     condition=parties.id == companies.party).select(
-                                     Max(banknumbers.id), Max(banknumbers.number_compact), companies.id,
-                                     where=((banknumbers.type == 'iban') &
-                                            (bankaccounts.active == True) &
-                                            (parties.active == True) &
-                                            (companies.is_Condominium == True) &
-                                            (companies.sepa_creditor_identifier != None)),
-                                     group_by=companies.id,
-                                     having=(Count(companies.id)==1)))
-
-        Date = pool.get('ir.date')
-        d = Date.today()
-        ddd = d.replace(day = 2,
-                        month = d.month+1 if d.month<12 else 1,
-                        year = d.year if d.month<12 else d.year+1)
-
-        #TODO: Check that condo has mandates
-        values = []
-        for (idb, number_compact, idc) in cursor.fetchall():
-            record = {
-                    'reference':          '{:04d}'.format(ddd.year) +
-                                          '_' +
-                                          '{:02d}'.format(ddd.month) +
-                                          '-' +
-                                          number_compact [8:12] +
-                                          '.' +
-                                          '{:04d}'.format(ddd.year)[-2:],
-                    'company':            idc,
-                    'account_number':     idb,
-                    'date':               ddd +
-                                          datetime.timedelta(days= 2 if ddd.weekday()>4 else 0),
-                    'sepa_charge_bearer': 'SLEV'
-                   }
-            values.append(record)
-
-        cls.create(values)
 
 
 class CondoPayment(Workflow, ModelSQL, ModelView):
@@ -813,7 +705,6 @@ class CondoPayment(Workflow, ModelSQL, ModelView):
     @fields.depends('group')
     def on_change_group(self):
         if self.group:
-            self.company = self.group.company
             self.currency = self.group.company.currency
             self.currency_digits = self.group.company.currency.digits or 2
             self.date = self.group.date
